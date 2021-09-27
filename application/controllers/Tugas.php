0ef60e801d3f206cb7d25c53145c9b549c8c40dd
hesapermana44 <hesapermana44@gmail.com>
2021-09-27 19:51:31 +0700

    Pertemuan1

---
+++ application/controllers/Tugas.php
@@ -0,0 +1,13 @@
+<?php
+
+$teks1="Belajar Menghitung Volume Kubus";
+$teks2="Panjang Sisi Kubus";
+$teks3="Volume Kubus";
+$teks4="cm3";
+$sisi=15;
+$volume=$sisi*$sisi*$sisi;
+
+echo "$teks1<br>";
+echo "$teks2 = $sisi<br>";
+echo "$teks3 = $volume $teks4<br>";
+?>
