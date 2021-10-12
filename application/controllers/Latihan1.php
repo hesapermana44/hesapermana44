f6bdedffb3c09a34120816cfdd1ce7e8127b80a3
hesapermana44 <hesapermana44@gmail.com>
2021-10-12 15:15:36 +0700

    Pertemuan 5

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -1,17 +1,30 @@
 <?php
+
 class latihan1 extends CI_Controller
 {
   public function index()
   {
-    echo "Selamat Datang.. selamat belajar Web Programing";
-    //$this->load->view('view-Latihan1');
+    echo "Selamat Datang... ";
+  }
+  public function penjumlahan($n1, $n2)
+	{
+    $this->load->model('model_latihan1');
+
+		$data['nilai1']= $n1;
+		$data['nilai2']= $n2;
+    $data['hasil'] = $this->model_latihan1->jumlah($n1, $n2);
+ //echo "Hasil Penjumlahan dari ". $n1 ." + ". $n2 ." = " .$hasil;
+ $this->load->view('view-latihan', $data);
+  }
+  public function pengurangan($n1, $n2)
+	{
+    $this->load->model('model_latihan1');
+
+		$data['nilai1']= $n1;
+		$data['nilai2']= $n2;
+    $data['hasil'] = $this->model_latihan1->kurang($n1, $n2);
+ //echo "Hasil Penjumlahan dari ". $n1 ." + ". $n2 ." = " .$hasil;
+ $this->load->view('view-pengurangan', $data);
   }
 
-  public function penjumlahan($n1, $n2)
-  {
-    $this-load->model('Model_Latihan1');
-    $hasil = $this->Model_Latihan1->jumlah($n1, $n2);
-    echo "Hasil Penjumlahan dari". $n1 ." + ". $n2 ." = "
-.$hasil;
-  }
-}
+ }
